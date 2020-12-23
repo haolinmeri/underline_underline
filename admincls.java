c04f247a6a40d6c1f62526d8eac745acb117f7f4
haolinmeri <lin.hao@meri.co>
2020-12-24 00:26:36 +0800

    Add files via upload

---
+++ admincls.java
@@ -0,0 +1,94 @@
+package project1;
+
+public class FoodCategory {
+	
+	public static String[][] foodQuestions=new String[20][6];
+	public static int foodQuestionPointer =0;
+	
+	public static void fillFoodArray() {
+		
+		foodQuestions[foodQuestionPointer][0]="Q1. What country produces the most potatoes?";
+		foodQuestions[foodQuestionPointer][1]="1. China";
+		foodQuestions[foodQuestionPointer][2]="2. United States";
+		foodQuestions[foodQuestionPointer][3]="3. Ireland";
+		foodQuestions[foodQuestionPointer][4]="4. Russia";
+		foodQuestions[foodQuestionPointer][5]="1";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q2. What soft-drink company introduced the brand Slice?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. Dr. Pepper\\n";
+		foodQuestions[foodQuestionPointer][2]="2. Coca Cola \\n";
+		foodQuestions[foodQuestionPointer][3]="3. Seven Up\\n";
+		foodQuestions[foodQuestionPointer][4]="4. Pepsico";
+		foodQuestions[foodQuestionPointer][5]="2";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q3. In the drink called a zombie, what is the main alcoholic ingredient?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. beer\\n";
+		foodQuestions[foodQuestionPointer][2]="2. brandy\\n";
+		foodQuestions[foodQuestionPointer][3]="3. rum\\n";
+		foodQuestions[foodQuestionPointer][4]="4. whiskey";
+		foodQuestions[foodQuestionPointer][5]="3";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q4. Of the following dishes, which are not typically made with some kind of seafood?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. Bouillabaisse\\n";
+		foodQuestions[foodQuestionPointer][2]="2. osso buco \\n";
+		foodQuestions[foodQuestionPointer][3]="3. fritto misto\\n";
+		foodQuestions[foodQuestionPointer][4]="4. tempura";
+		foodQuestions[foodQuestionPointer][5]="2";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q5. Which of the following ingredients are not used in a Bloody Mary according to Playboy Bar Guide?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. ketchup\\n";
+		foodQuestions[foodQuestionPointer][2]="2. sugar \\n";
+		foodQuestions[foodQuestionPointer][3]="3. Tabasco sauce\\n";
+		foodQuestions[foodQuestionPointer][4]="4. Worcestershire sauce";
+		foodQuestions[foodQuestionPointer][5]="2";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q6. The original Bellini was a mixture of sparkling Italian white wine and what type of fruit juice?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. apple\\n";
+		foodQuestions[foodQuestionPointer][2]="2. orange\\n";
+		foodQuestions[foodQuestionPointer][3]="3. peach \\n";
+		foodQuestions[foodQuestionPointer][4]="4. pomegranate";
+		foodQuestions[foodQuestionPointer][5]="3";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q7. The sandwich known as the \\\"Reuben\\\" does not have which of the following ingredients?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. boiled ham \\n";
+		foodQuestions[foodQuestionPointer][2]="2. corned Beef\\n";
+		foodQuestions[foodQuestionPointer][3]="3. sauerkraut\\n";
+		foodQuestions[foodQuestionPointer][4]="4. Swiss Cheese";
+		foodQuestions[foodQuestionPointer][5]="1";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q8. Marzipan is made with what kind of nut?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. almond \\n";
+		foodQuestions[foodQuestionPointer][2]="2. cashew\\n";
+		foodQuestions[foodQuestionPointer][3]="3. pecan\\n";
+		foodQuestions[foodQuestionPointer][4]="4. walnut";
+		foodQuestions[foodQuestionPointer][5]="1";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q9. Which of the following is not a favorable adjective when discussing wine?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. fat \\n";
+		foodQuestions[foodQuestionPointer][2]="2. flinty\\n";
+		foodQuestions[foodQuestionPointer][3]="3. leggy\\n";
+		foodQuestions[foodQuestionPointer][4]="4. vigorous";
+		foodQuestions[foodQuestionPointer][5]="1";
+		foodQuestionPointer ++;
+		
+		foodQuestions[foodQuestionPointer][0]="\\n Q10. Of all commercial cooking oils, which of these is highest in polyunsaturates  and lowest in saturated fat?\\n";
+		foodQuestions[foodQuestionPointer][1]="1. coconut oil\\n";
+		foodQuestions[foodQuestionPointer][2]="2. corn oil\\n";
+		foodQuestions[foodQuestionPointer][3]="3. olive oil\\n";
+		foodQuestions[foodQuestionPointer][4]="4. safflower oil";
+		foodQuestions[foodQuestionPointer][5]="4";
+		foodQuestionPointer ++;
+		
+		
+		
+	}
+	
+}
